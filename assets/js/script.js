0641bb88336c24118130cf9a33fcfed398d46363
BrennenMcGill <31702142+BrennenMcGill@users.noreply.github.com>
2020-05-20 21:24:00 -0600

    made the edit button work

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,11 +98,30 @@ var taskFormHandler = function() {
   var taskButtonHandler = function(event) {
     console.log(event.target);
 
-    if (event.target.matches(".delete-btn")) {
+    if (event.target.matches(".edit-btn")) {
+      var taskId = event.target.getAttribute("data-task-id");
+      editTask(taskId);
+    }
+
+    else if (event.target.matches(".delete-btn")) {
       var taskId = event.target.getAttribute("data-task-id");
       deleteTask(taskId);
     }
   };
+
+  var editTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+
+    // get content from task name and type
+    var taskName = taskSelected.querySelector("h3.task-name").textContent;
+
+    var taskType = taskSelected.querySelector("span.task-type").textContent;
+
+    document.querySelector("input[name='task-name']").value = taskName;
+    document.querySelector("select[name='task-type']").value = taskType;
+    document.querySelector("#save-task").textContent = "Save Task";
+    formEl.setAttribute("data-task-id", taskId);
+  }
 
   var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
